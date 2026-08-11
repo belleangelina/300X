78e0ac840f0f8ac55da9da397c660293752993b0
belleangelina <125722780+belleangelina@users.noreply.github.com>
2026-08-11 13:24:25 +0800

    Invalidate caches for directory parser updates

--- lib/features/library/data/work_index_repository.dart
+++ lib/features/library/data/work_index_repository.dart
@@ -24,7 +24,7 @@ class WorkIndexRecord
 
 class WorkIndexRepository
 {
-    static const int currentResolverVersion = 25;
+    static const int currentResolverVersion = 26;
 
     WorkIndexRepository(this._database, [this._workCodec = const WorkCodec()]);
 
